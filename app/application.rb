33cb17201a5b9107e20e3bd48fcd0032d38a65c0
Nataliya R <n.rudenko@gmail.com>
2019-05-15 19:38:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      elsif req.path.match(/item)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end 
       
     else
       resp.write "Path Not Found"
